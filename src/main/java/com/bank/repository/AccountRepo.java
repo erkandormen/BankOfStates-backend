2b3c6a4f6123c5bc5bc1cace33aae552808e6387
erkan dormen <ekstremyazilim@gmail.com>
2021-06-14 23:30:43 +0300

    first commit

---
+++ src/main/java/com/bank/repository/AccountRepo.java
@@ -0,0 +1,14 @@
+package com.bank.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.bank.model.Account;
+
+public interface AccountRepo extends CrudRepository<Account, Long> {
+	
+	Account findByAccountNumber(Long accountNumber);
+	
+	List<Account> findAll();
+}
